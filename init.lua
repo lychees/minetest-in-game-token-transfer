323bc1da35c1242f33c10d5ee2f0817c60a6440a
xiaodao <lychees67@gmail.com>
2022-06-05 07:49:41 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -8,6 +8,11 @@ local modpath = minetest.get_modpath(modname)
 assert(loadfile(modpath.. "/settings.lua"))(modpath)
 
 local _contexts = {}
+local function get_context(name)
+    local context = _contexts[name] or {}
+    _contexts[name] = context
+    return context
+end
 
 --Aunction for copy a table in deep (children too)
 local function deepCopy(original)
@@ -196,10 +201,18 @@ end
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 
     if formname == "player:interact" then
+		local s = player:get_player_name()
+		local t = get_context(s).target		
 		if fields.profile then			
-			minetest.chat_send_player(player:get_player_name(), ".EM_ASM window.open(\"https://" .. player:get_player_name() .. ".test.w3itch.io/zh-CN\", \"new\")")							
+			minetest.chat_send_player(s, ".EM_ASM window.open(\"https://" .. t .. ".test.w3itch.io/zh-CN\", \"new\")")							
 		end
-		return
+		if fields.transfer then			
+			minetest.chat_send_player(s, ".EM_ASM alert(feature not implement yet...)")							
+		end
+		if fields.follow then			
+			minetest.chat_send_player(s, ".EM_ASM alert(feature not implement yet...)")							
+		end
+		return true
     end
     
 	if formname ~= "pickp:form" then
@@ -320,35 +333,45 @@ local function pickpocketing(clicker, clicked)
 	end
 end
 
-minetest.register_on_rightclickplayer(function(player, clicker)
-	local controls = clicker:get_player_control()
-	if not(check_distance(clicker, player)) then
-		minetest.chat_send_player(clicker:get_player_name(), S("Target too far."))
-		return
-	end
-	-- pickpocketing(clicker, player)
-    local formspec = {
-        "formspec_version[4]",
-        "size[6,3.476]",
-        "button[1.5,2.3;3,0.8;profile;Profile]"
-		"button[1.5,2.3;3,0.8;transfer;Transfer]"
-		"button[1.5,2.3;3,0.8;follow;Follow]"
-		"button[1.5,2.3;3,0.8;cancel;Cancel]"
-    }
-	minetest.show_formspec(clicker:get_player_name(), "player:interact", formspec)
-	-- minetest.chat_send_player(clicker:get_player_name(), ".EM_ASM window.open(\"https://" .. player:get_player_name() .. ".test.w3itch.io/zh-CN\", \"new\")")	
+minetest.register_on_leaveplayer(function(player)
+    _contexts[player:get_player_name()] = nil
 end)
 
+minetest.register_on_rightclickplayer(function(player, clicker)
+	local s = clicker:get_player_name()
+	local t = player:get_player_name()
+	local controls = clicker:get_player_control()
+	if not(check_distance(clicker, player)) then
+		minetest.chat_send_player(s, S("Target too far."))
+		return
+	end
+	local context = get_context(s)
+	context.target = t	
+	local formspec = {
+		"formspec_version[4]",
+		"size[4.5,5.25]",
+		"label[0.375,0.5;", minetest.formspec_escape("This is " .. t .. "."), "]",
+		"button_exit[0.5,1;3.5,0.8;profile;Profile]",
+		"button_exit[0.5,2;3.5,0.8;transfer;Transfer]",
+		"button_exit[0.5,3;3.5,0.8;follow;Follow]",
+		"button_exit[0.5,4;3.5,0.8;cancel;Cancel]"
+	}
+	minetest.show_formspec(s, "player:interact", table.concat(formspec, ""))
+end)
 
-minetest.register_chatcommand("go", {
-    local formspec = {
-        "formspec_version[4]",
-        "size[6,3.476]",
-        "button[1.5,2.3;3,0.8;profile;Profile]"
-		"button[1.5,2.3;3,0.8;transfer;Transfer]"
-		"button[1.5,2.3;3,0.8;follow;Follow]"
-		"button[1.5,2.3;3,0.8;cancel;Cancel]"
-    }
-	minetest.show_formspec(clicker:get_player_name(), "player:interact", formspec)
-	-- minetest.chat_send_player(clicker:get_player_name(), ".EM_ASM window.open(\"https://" .. player:get_player_name() .. ".test.w3itch.io/zh-CN\", \"new\")")	
+minetest.register_chatcommand("go", {	
+    func = function(name)
+		local context = get_context(name)
+		context.target = name
+		local formspec = {
+			"formspec_version[4]",
+			"size[4.5,5.25]",
+			"label[0.375,0.5;", minetest.formspec_escape("This is " .. name .. "."), "]",
+			"button_exit[0.5,1;3.5,0.8;profile;Profile]",
+			"button_exit[0.5,2;3.5,0.8;transfer;Transfer]",
+			"button_exit[0.5,3;3.5,0.8;follow;Follow]",
+			"button_exit[0.5,4;3.5,0.8;cancel;Cancel]"
+		}
+		minetest.show_formspec(name, "player:interact", table.concat(formspec, ""))
+    end,
 })
